a76aad89762d48203ca3f2e6f7e1f6c99361f496
michaell pulido <katerinaguilar12345@gmail.com>
2026-06-04 13:25:05 -0500

    minecraft

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CharacterService, Character } from './core/services/character.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -12,11 +14,14 @@ export class App implements OnInit {
   protected readonly title = signal('rick3');
   private characterService = inject(CharacterService);
 
+  characters: Character[] = [];
+
   ngOnInit(): void {
     console.log('Iniciando fetching de datos...');
     this.characterService.getCharacters().subscribe({
       next: (response) => {
         console.log('¡Conexión exitosa! Datos obtenidos:', response);
+        this.characters = response.results;
       },
       error: (error) => {
         console.error('Error al obtener los datos:', error);
